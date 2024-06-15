eae86ccd3475f466b4755ddf87c650db4515ba4c
ahiru60 <adhirunath@gmail.com>
2024-06-15 08:47:53 +0530

    search section color change function

--- app/src/main/java/com/example/ar_furniture_application/MainActivity.java
+++ app/src/main/java/com/example/ar_furniture_application/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.ar_furniture_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.ar_furniture_application.Home_Fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton homeImgButton,searchImgButton,notifImgButton,profileImgButton;
+    ImageButton homeImgButton,searchImgButton,notifImgButton,profileImgButton,topSearchImageButton;
     View mContentView;
+    ConstraintLayout searchSection;
+    EditText searchEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         searchImgButton = findViewById(R.id.search_imageBtn);
         notifImgButton= findViewById(R.id.notification_imageBtn);
         profileImgButton= findViewById(R.id.profile_imageBtn);
+        searchSection = findViewById(R.id.search_section);
+        searchEditText = findViewById(R.id.searchEditText);
+        topSearchImageButton = findViewById(R.id.topSearchImageButton);
 
         homeImgButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,21 +44,24 @@ public class MainActivity extends AppCompatActivity {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView, HomeFragment.class, null)
                         .setReorderingAllowed(true)
-                        .addToBackStack("name") // Name can be null
+                        .addToBackStack("home") // Name can be null
                         .commit();
-
+                setSearchSectionColors(false);
             }
         });
 
         searchImgButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView, SearchFragment.class, null)
                         .setReorderingAllowed(true)
-                        .addToBackStack("name") // Name can be null
+                        .addToBackStack("search") // Name can be null
                         .commit();
+
+                setSearchSectionColors(true);
 
             }
         });
@@ -61,9 +73,9 @@ public class MainActivity extends AppCompatActivity {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView, NotificationsFragment.class, null)
                         .setReorderingAllowed(true)
-                        .addToBackStack("name") // Name can be null
+                        .addToBackStack("notification") // Name can be null
                         .commit();
-
+                setSearchSectionColors(false);
             }
         });
 
@@ -74,10 +86,25 @@ public class MainActivity extends AppCompatActivity {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView, ProfileFragment.class, null)
                         .setReorderingAllowed(true)
-                        .addToBackStack("name") // Name can be null
+                        .addToBackStack("profile") // Name can be null
                         .commit();
-
+                setSearchSectionColors(false);
             }
         });
+    }
+
+    void setSearchSectionColors(boolean state){
+        if(state==true){
+        searchSection.setBackground(getDrawable(R.drawable.background_transparent_shape));
+        searchEditText.setTextColor(getResources().getColor(R.color.black, null));
+        searchEditText.setHintTextColor(getResources().getColor(R.color.black, null));
+        searchEditText.setBackground(getDrawable(R.drawable.background_round_edged_black_shape));
+        topSearchImageButton.setImageResource(R.drawable.ic_search_black);}
+        else{
+            searchSection.setBackground(getDrawable(R.drawable.background_gradient_shape));
+            searchEditText.setTextColor(getResources().getColor(R.color.white, null));
+            searchEditText.setHintTextColor(getResources().getColor(R.color.white, null));
+            searchEditText.setBackground(getDrawable(R.drawable.background_round_edged_white_shape));
+            topSearchImageButton.setImageResource(R.drawable.ic_search_white);}
     }
 }
